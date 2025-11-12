18a4c22c31e8dcc5220d3604a87861de03cdbb6a
omarhicham44-ux <omarhicham44@gmail.com>
2025-11-12 15:20:53 +0200

    Define Enclosure class with animal management methods

--- Enclosure.h
+++ Enclosure.h
@@ -3,7 +3,25 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure
+{
+private :
+	Animal* animals;
+	int capacity;
+	int current count;
+public:
+	Enclosure();
+	Enclosure(Animal* a; int ca; int cu);
+	~Enclosure();
+	void addAnimal(Animal* a);
+	void displayAnimals();
+	Animals* getAnimals();
+	void SetAnimals(Animals* a);
+	int Getca();
+	void Setca(int a);
+	int Getcu();
+	void SetCu();
+};
 
 
 
