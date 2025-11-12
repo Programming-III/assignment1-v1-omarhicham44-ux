34771ccd6d497b5b027417e9810f9a632895285c
omarhicham44-ux <omarhicham44@gmail.com>
2025-11-12 15:20:17 +0200

    Add Bird class definition with wingspan
    Implemented Bird class with wingspan attribute and methods.

--- Bird.h
+++ Bird.h
@@ -3,7 +3,17 @@
 
 #include "Animal.h"
 
-//define class bird here 
+class Bird : public Animal
+{
+private:
+	float wingspan;
+public:
+	Bird();
+	Bird(string n; int a; bool h; float w) :Animal(n; a; h);
+	~Bird();
+	float GetWings();
+	void SetWingd(float f);
+}
 
 
 
